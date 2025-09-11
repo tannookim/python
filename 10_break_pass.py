507588e84d1f8f7b6418ee7aceed9b1d9f9b4da2
김성은 <tannookim@gmail.com>
2025-09-11 11:28:56 +0900

    break문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -11,22 +11,24 @@ running = True
 # print('while 문 종료')
 
 for i in range(1,10):         # 변수 i의 범위는 1~9까지
-    if i ==3:                 # 변수 i의 값이 3이라면
-        continue              # 위 조건 무시하고 지나가
-    if i ==6:                 # 변수 i의 값이 6이라면
-        continue
-    if i ==9:                 # 변수 i의 값이 9이라면
+     if i ==3:                 # 변수 i의 값이 3이라면
+         continue              # 위 조건 무시하고 지나가
+     if i ==6:                 # 변수 i의 값이 6이라면
+         continue
+     if i ==9:                 # 변수 i의 값이 9이라면
+         continue
+     print(i)
+
+
+#이렇게 할 수 있음.
+#1.
+for i in range(1,10):
+    if i == 3 or i == 6 or i == 9:   # 변수 i가 3 또는 6 또는 9 라면
         continue
     print(i)
 
-
-
+#2.
 for i in range(1,10):
-    if i ==3 | i ==6 | i ==9 :
-        continue
-    print(i)
-
-for i in range(1,10):
-    if i %3 == 0:
+    if i %3 == 0:                    #변수 i를 3을 나눴을 때 나머지 값이 0이라면
         continue
     print(i)
