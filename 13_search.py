cb89194017af0a72f4601471e8aaa861ec8fd3c5
김성은 <tannookim@gmail.com>
2025-09-11 15:47:55 +0900

    search

--- 13_search.py
+++ 13_search.py
@@ -26,11 +26,58 @@ print(f'3은 어디에?:{b.index(3)},{b.index(3,4)},{b.index(3,9)}번 인덱스'
 
 idx = 0
 # while True:
-#     idx = b.index(3,idx)      ##리스트 b에서 변수 idx 값부터 숫자 3을
-#     print(f'3의 값은 {idx}번에 있다.')
-#     idx += 1
+#     idx = b.index(3,idx)      ##리스트 b에서 변수 idx 값부터 숫자 3을 idx번 인덱스부터 찾아라
+#       print(f'3의 값은 {idx}번에 있다.')  ##몇번 인덱스에 있는지 출력
+#     idx += 1  ##찾은 인덱스 번호에 1을 더하고 idx = b.index(3,idx) 반복 => 마지막 3의 인덱스 번호까지 찾고나면 더이상 3이 없다는 에러 메세지가 뜸.
 
 for n in b:  # for in 을 이용하면 list에 있는 값을 순서대로 하나씩 뽑아낸다.
     if n == 3:
         print(f'3이 있는 인덱스 : {idx}')
     idx += 1
+
+# 리스트 요소 삭제
+# del a[3]과 a.remove(3) 의 차이
+# del은 특정 인덱스의 값을 지운다.
+# remove는 해당 값을 지운다.(처음 찾은 것만 지운다)
+print(f'a:{a}')
+a.remove(3)
+print(f'a:{a}')
+
+# pop() = append()의 반대개념
+# 맨마지막 요소를 빼낸다.(리스트에서는 사라진다.)
+val = a.pop()
+print(f'빼낸 값 : {val} / a: {a}')
+val = a.pop()
+print(f'빼낸 값 : {val} / a: {a}')
+
+# 리스트 확장(더하기와 비슷한 개념)
+print(a)
+a.extend(b)    ##리스트에 a에 b를 더(확장)한다.
+print(a)
+
+# count(val) 특정한 값이 해당 리스트에 몇개 있는지 확인해주는 것
+print(a)
+print(f'a안에 3인 값은 {a.count(3)}개 있다.')  ##리스트 a에 '3' 몇개 있는지 출력
+print(f'a안에 9인 값은 {a.count(9)}개 있다.')  # 없는 값은 0을 반환
+
+# a 안에 있는 모든 3을 지워주세요.
+#print(a)
+#idx = 0
+#for n in a:
+    #if n == 3:
+        #del a[idx]
+    #idx += a.index(n)
+    #print(a)                    ##여기까지 내가 해본거 => 실패작!
+
+
+# 이렇게 할 수 있다.
+# for n in a:            ##리스트 a에서 값을 하나씩 뽑아서 n이라는 변수에 대입할거야
+#     if n == 3:         ##n값이 3과 같다면
+#         a.remove(3)    ##리스트 a에서 '3' 삭제
+# print(a)
+
+while True:
+    a.remove(3)          ##리스트 a에서 '3' 삭제
+    if a.count(3) == 0:  ##리스트 a에서 '3'의 개수가 0과 같다면
+        break            ##멈춰
+print(a)
