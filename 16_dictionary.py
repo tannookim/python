87a13626c31f04916965a57234c8bcda3853c7b2
김성은 <tannookim@gmail.com>
2025-09-12 11:15:13 +0900

    dic

--- 16_dictionary.py
+++ 16_dictionary.py
@@ -24,3 +24,11 @@ print(a)
 # 사전에서 특정 요소 삭제
 del a['second']
 print(a)
+
+# 사전의 특정 요소를 꺼내보자(사용법은 list와 비슷하다)
+print(dic2['name'])
+print(dic2['friends'])
+# get 매서드(함수???)를 활용해서도 가져올 수 있다.
+print(dic2.get('phone'))
+# 특정 키가 없는 경우 None이 아닌 대체 내용으로 반환 할 수 있음
+print(dic2.get('nick','해당 내용이 없음'))
