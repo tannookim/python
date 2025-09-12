38c51c42e1ff1802bde170883f8f64175b8e4180
김성은 <tannookim@gmail.com>
2025-09-12 11:48:06 +0900

    dic 함수소개

--- 17_dic_func.py
+++ 17_dic_func.py
@@ -3,3 +3,20 @@ dic = {
     'phone':'55555555',
     'friends':['Alice','Smith','John']
 }
+
+# dic.keys() : 특정한 사전의 키들만 가져와 dict_keys라는 객체를 반환한다.
+print(dic.keys())
+
+for item in dic.keys():
+    print(item)
+
+# dict_keys -> list로 변환  ##dict_keys를 list로 변경해서 keys 라는 변수에 담는 소스
+keys = list(dic.keys())
+print(keys)
+
+# dic.values() : 특정 사전의 값만 가져와 dict_values 라는 객체를 반환
+print(dic.values())
+
+# list로 변경해서 values 라는 변수에 담아보자
+values = list(dic.values())
+print(values)
