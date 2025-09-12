686a40f09211ec099276d2664d8484adaaaf7269
김성은 <tannookim@gmail.com>
2025-09-12 13:06:14 +0900

    set 소개

---
+++ 18_set.py
@@ -0,0 +1,8 @@
+# set은 순서가 없고, 중복을 허용하지 않는다.
+number_set = set([1,2,3])
+print(number_set)          ## dictionary와 비슷하게 생겼지만 key 없고 값만 존재함.
+
+# 중복을 제외하고 담는다.
+# 그래서 중복 제거용도로 사용된다.
+str_set = set("HelloWorld")
+print(str_set)
