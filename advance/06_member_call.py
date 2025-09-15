5eb1beb68f2a16e23a6262083c1885a0275e0c0c
김성은 <tannookim@gmail.com>
2025-09-15 16:00:32 +0900

    멤버 호출

---
+++ advance/06_member_call.py
@@ -0,0 +1,16 @@
+class Car:
+    # gear,on은 멤버 변수 또는 멤버 필드라고 한다.
+    gear = 0
+    on = False
+
+    # 생성자
+    def __init__(self):
+        pass
+
+    #멤버 함수 - 클래스 안의 생성자 함수들은 해당 객체를 표시하기 위한 self를 기본으로 가지고 있는다.
+    def start(self):
+        pass
+
+    def change(self):
+        pass
+
