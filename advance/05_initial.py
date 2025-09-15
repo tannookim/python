fd739498179fbf66af026e5a7303ad014b8dddec
김성은 <tannookim@gmail.com>
2025-09-15 14:34:13 +0900

    클래스 초기화

--- advance/05_initial.py
+++ advance/05_initial.py
@@ -1,7 +1,14 @@
 class Puppy:
 
-    name=""
+    name="" # 멤버 변수(필드)  : class 안에서 사용 가능한 변수
     goal=""
 
-    def __init__(self,name,goal):
-        pass
+    def __init__(self,name,goal): # 생성자 : 객체화시 호출되는 함수  ##객체화 될때 딱 한번만 불러지는 애
+        # 받아온 name 과 goal 은 이 생성자르 벗어날수 없다.(생성자의 쓰임이 다하면 함께 없어진다.)
+        # 그래서 클래스(객체) 멤버 에다가 넣어줘야, 객체가 살아있는 동안 사용이 가능 하다.
+        # 그런데  name = name 형태로는 어떤것이 객체의 멤버인지 알 수 없다.
+        # 그래서 멤버인 녀석은 self 를 이용하여 표시해 준다.
+        self.name=name       ##여기서 self는 현재의 객체를 의미한다????????????
+        self.goal=goal
+
+puppy = Puppy("멍멍이","집지키기")
