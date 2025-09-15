042a92b41909b8051eee05dffc0373be9d7f8800
김성은 <tannookim@gmail.com>
2025-09-15 10:07:03 +0900

    함수 인자값-튜플로 받기

--- 21_function_args.py
+++ 21_function_args.py
@@ -6,3 +6,15 @@ def plus(num=0):
 print(plus(5)) # => 10
 print(plus()) # TypeError: plus() missing 1 required positional argument: 'num' ##값 한개는 있어야 하는데 왜 안넣었니
 
+# 인자값의 종류를 튜플(수정이 불가능한 list 형태)로만 받겠다. ##인자값(매개변수) 앞에 * 붙이는거
+def tuple_args(*numbers):
+    print(numbers)
+    total = 0
+    for num in numbers:
+        total += num
+    return total      ##return은 함수 1개당 1개만 들어갈 수 있는데 total 아래로 들여쓰기 되어 있으면 for문에 걸려서 5번 나오는 에러.
+
+# 이 방식은 사용자가 인자값의 갯수를 자유롭게 정해서 넣을 수 있다.
+print(tuple_args(1,2,3,4,5))
+
+
