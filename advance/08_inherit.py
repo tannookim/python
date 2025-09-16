2eb3369d400abc1bf663e3e05d0b8d8046dbc04b
김성은 <tannookim@gmail.com>
2025-09-16 12:08:20 +0900

    클래스 생성 마무리

--- advance/08_inherit.py
+++ advance/08_inherit.py
@@ -1,18 +1,20 @@
 ##상속의 개념 : 내것처럼 사용한다.
 class Runner:             ##생성자는 당연히 있어야 하는거라서 생략함.
     def run(self):
-        pass
+        print('달린다.')
 
     def sprint(self):
-        pass
+        print('전력질주를 한다.')
 
 
 class Jumper:
     def jump(self):
-        pass
+        print('점프를 한다.')
 
     def high_jump(self):
-        pass
+        print('높이 점프를 한다.')
 
-class Person(Jumper, Runner):
-    pass
+
+class Person(Jumper, Runner): # Jumper 와 Runner 를 상속 받았다.
+    def walk(self):
+        print('걷는다.')
